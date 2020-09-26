c7c0c97304bfd674343c44c92f8061c3fe3a61b8
vagrant <vagrant@localhost.localdomain>
2020-09-26 08:08:48 +0000

    fix:mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,16 @@ Rails.application.configure do
 
   config.assets.paths << Rails.root.join("app", "assets", "fonts")
   config.assets.precompile += %w( .svg .eot .woff .ttf .jpeg .png .jpg )
+
+  #お問い合わせ
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:             ENV['INQUIRY_MAIL'],
+  password:              ENV['INQUIRY_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto:  true
+  }
 end
