ec5087200cf9913cfbb7712b430aebb8f7fad2c3
vagrant <vagrant@localhost.localdomain>
2020-09-28 13:20:17 +0000

    deploy

--- app/models/custom.rb
+++ app/models/custom.rb
@@ -6,7 +6,12 @@ class Custom < ApplicationRecord
 	validates :task, presence: true, length:{maximum:20}
 	validates :task_status, presence: true
 
-
 	enum	task_status: [:未達成, :達成]
 
+	def self.task_complete
+		unless self.count == 0
+			@task = self.pluck(:task_status)
+			@task.all?{|task| task == "達成"}
+		end
+	end
 end
