16ceadf978ac2fb20a52833758137bbc765b4df5
vagrant <gomisuke1998@gmail.com>
2020-10-20 14:01:20 +0000

    fix:driver

--- spec/system/diaries_spec.rb
+++ spec/system/diaries_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe "Diaries", type: :system do
   	describe "日記新規登録機能のテスト" do
 
-  		context "フォーム入力値が正常" do
-  			let(:genre) {create(:genre)}
+  		let(:genre) {create(:genre)}
 
-			it "日記の新規作成が成功" do
+  		context "フォーム入力値が正常の場合" do
+
+			it "日記の新規作成が成功する" do
 				visit new_diary_path
 				fill_in "日付", with: "2020-01-01"
 				fill_in "タイトル", with: "テスト"
@@ -15,6 +16,12 @@ RSpec.describe "Diaries", type: :system do
 				fill_in "充実度（気分）", with: 1
 				click_button "日記作成"
 				expect(page).to have_selector "alert-success", text: "日記を作成しました！"
+			end
+		end
+
+		context "フォームの入力値が不正の場合" do
+
+			it "日記の新規作成が失敗する" do
 			end
 		end
 	end
