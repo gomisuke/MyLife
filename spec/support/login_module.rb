ea6f835a6c7561b2ff72e3a939d21358b75293f7
vagrant <gomisuke1998@gmail.com>
2020-10-22 07:04:22 +0000

    add:test:diary_new

---
+++ spec/support/login_module.rb
@@ -0,0 +1,5 @@
+module LoginModule
+	def login(user)
+		visit login_path
+		fill_in 'email', with: user.email
+		fill_in 'password', with: ''
--- spec/system/diaries_spec.rb
+++ spec/system/diaries_spec.rb
@@ -2,20 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Diaries", type: :system do
   	describe "日記新規登録機能のテスト" do
+  		let!(:genre) {create(:genre)}
+  		#let(:user) {create(:user)}
 
-  		let(:genre) {create(:genre)}
+  		before do
+  			visit new_user_session_path
+  			fill_in 'メールアドレス', with: 'test@example.com'
+  			fill_in 'パスワード', with: '000000'
+  			click_button 'ログイン'
+  		end
 
   		context "フォーム入力値が正常の場合" do
 
 			it "日記の新規作成が成功する" do
 				visit new_diary_path
-				#fill_in "diary", with: "2020-01-01"
-				fill_in 'diary[title]', with: "テスト"
+				fill_in "日付", with: "2020-01-01"
+				fill_in 'タイトル', with: "テスト"
 				fill_in "内容", with: "日記新規登録機能のテスト"
-				fill_in "ジャンル", with: genre.id
-				fill_in "充実度（気分）", with: 1
+				select genre.name, from: 'ジャンル'
+				choose "絶不調"
 				click_button "日記作成"
-				expect(page).to have_selector "alert-success", text: "日記を作成しました！"
+				expect(page).to have_content "日記を作成しました！"
 			end
 		end
 
