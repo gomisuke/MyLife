e9651981e6446a022155e54465dffcf8c44979cc
vagrant <gomisuke1998@gmail.com>
2020-10-24 23:20:25 +0000

    add:test:custom

---
+++ spec/system/customs_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Customs", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe "習慣タスクの新規登録" do
+  	let!(:user) {create(:user)}
+
+  	before do
+  		login(:user)
+  	end
+
+  	context "フォーム入力値が正常の場合" do
+
+  		it "新規タスク登録が成功する" do
+  			visit customs_management_path
+  			fill_in "新しい習慣を作る", with: "テスト"
+  			click_button "作成"
+  			expect(page).to have_content "テスト"
+  		end
+  	end
+  end
+
+end
