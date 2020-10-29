c5f17812a715a5c367474dc638f165206a19c4c6
vagrant <gomisuke1998@gmail.com>
2020-10-29 09:00:40 +0000

    add:test:diary

--- spec/system/diaries_spec.rb
+++ spec/system/diaries_spec.rb
@@ -4,13 +4,10 @@ RSpec.describe "Diaries", type: :system do
 
   	describe "日記新規登録機能のテスト" do
   		let!(:genre) {create(:genre)}
-
   		before do
   			login(genre.user)
   		end
-
   		context "フォーム入力値が正常の場合" do
-
 			it "日記の新規作成が成功する" do
 				visit new_diary_path
 				fill_in "日付", with: "2020-01-01"
@@ -22,9 +19,7 @@ RSpec.describe "Diaries", type: :system do
 				expect(page).to have_content "日記を作成しました！"
 			end
 		end
-
 		context "フォームの入力値が不正の場合" do
-
 			it "日記の新規作成が失敗する" do
 				visit new_diary_path
 				fill_in "日付", with: "2020-01-01"
@@ -37,54 +32,76 @@ RSpec.describe "Diaries", type: :system do
 			end
 		end
 	end
-
-	describe "日記一覧表示のテスト" do
+	describe "一覧ページのテスト" do
 		let!(:user_a) {create(:user, name: "ユーザーA", email: "a@example.com", acount_name: "@test_user1")}
 		let!(:user_b) {create(:user, name: "ユーザーB", email: "b@example.com", acount_name: "@test_user2")}
 		let(:genre_a) {create(:genre, user: user_a)}
 		let(:genre_b) {create(:genre, user: user_b)}
-
 		before do
 			create(:diary, title: "日記A", genre: genre_a)
 			create(:diary, title: "日記B", genre: genre_b)
 		end
-
-		context "ユーザーAがログインしている場合" do
-
+		describe "ログインユーザーの投稿のみが表示される" do
+			context "ユーザーAがログインしている場合" do
+				before do
+					login(user_a)
+				end
+				it "ユーザーAの日記のみが表示される" do
+					visit diaries_path
+					expect(page).to have_content "日記A"
+				end
+				it "ユーザーA以外の日記が表示されない" do
+					visit diaries_path
+					expect(page).not_to have_content "日記B"
+				end
+			end
+			context "ユーザーBがログインしている場合" do
+				before do
+					login(user_b)
+				end
+				it "ユーザーBの日記のみが表示される" do
+					visit diaries_path
+					expect(page).to have_content "日記B"
+				end
+				it "ユーザーB以外のニキが表示されない" do
+					visit diaries_path
+					expect(page).not_to have_content "日記A"
+				end
+			end
+		end
+		describe "表示のテスト" do
 			before do
 				login(user_a)
-			end
-
-			it "ユーザーAの日記のみが表示される" do
 				visit diaries_path
+			end
+			it "タイトルが表示される" do
 				expect(page).to have_content "日記A"
 			end
-
-			it "ユーザーA以外の日記が表示されない" do
-				visit diaries_path
-				expect(page).not_to have_content "日記B"
+			it "日記内容が表示される" do
+				expect(page).to have_content "testtest"
 			end
-
-
+			it "日記投稿日時が表示される" do
+				expect(page).to have_content "2020年01月01日"
+			end
 		end
+	end
+	describe "詳細ページのテスト" do
+		let(:diary) {create(:diary)}
 
-		context "ユーザーBがログインしている場合" do
-
-			before do
-				login(user_b)
-			end
-
-			it "ユーザーBの日記のみが表示される" do
-				visit diaries_path
-				expect(page).to have_content "日記B"
-			end
-
-			it "ユーザーB以外のニキが表示されない" do
-				visit diaries_path
-				expect(page).not_to have_content "日記A"
-			end
-
+		before do
+			login(diary.user)
 		end
-
+		it "タイトルが表示される" do
+			visit diary_path(diary.id)
+			expect(page).to have_content diary.title
+		end
+		it "日記内容が表示される" do
+			visit diary_path(diary.id)
+			expect(page).to have_content diary.body
+		end
+		it "日記投稿日時が表示される" do
+			visit diary_path(diary.id)
+			expect(page).to have_content diary.diary_date.strftime("%Y年%m月%d日")
+		end
 	end
 end
