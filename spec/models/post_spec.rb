e8a1381fc008650b1aed027e6b35c940974ab3a2
vagrant <gomisuke1998@gmail.com>
2020-11-03 04:25:13 +0000

    add:test:post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,28 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-	it "ユーザーIDと投稿内容と投稿写真と公開設定があれば登録できる" do
-	    post = create(:post)
-	    post.valid?
-	    expect(post).to be_valid
-	end
 
-	it "ユーザーIDがなければ登録できない" do
-		post = build(:post, user_id: nil)
-		post.valid?
-		expect(post.errors[:user_id]).to include("を入力してください。")
-	end
+	describe "バリデーションのテスト" do
+		it "ユーザーIDと投稿内容と投稿写真と公開設定があれば登録できる" do
+		    post = create(:post)
+		    post.valid?
+		    expect(post).to be_valid
+		end
 
-	it "投稿内容がなければ登録されない" do
-		post = build(:post, content: nil)
-		post.valid?
-		expect(post.errors[:content]).to include("を入力してください。")
-	end
+		it "ユーザーIDがなければ登録できない" do
+			post = build(:post, user_id: nil)
+			post.valid?
+			expect(post.errors[:user_id]).to include("を入力してください。")
+		end
 
-	it "投稿写真がなければ登録できない" do
-		post = build(:post, post_image: nil)
-		post.valid?
-		expect(post.errors[:post_image]).to include("を入力してください。")
+		it "投稿内容がなければ登録されない" do
+			post = build(:post, content: nil)
+			post.valid?
+			expect(post.errors[:content]).to include("を入力してください。")
+		end
+
+		it "投稿内容が30文字以内でないと登録できない。" do
+			post = build(:post, content: "a" * 31)
+			post.valid?
+			expect(post.errors[:content]).to include("は30文字以内で入力してください。")
+		end
+
+		it "投稿写真がなければ登録できない" do
+			post = build(:post, post_image: nil)
+			post.valid?
+			expect(post.errors[:post_image]).to include("を入力してください。")
+		end
 	end
 
 
