a21ad630bf9cace8b0c7e85d0de7a28b72b1e9dc
vagrant <gomisuke1998@gmail.com>
2020-11-06 13:36:40 +0000

    add:test:post

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -10,16 +10,13 @@ RSpec.describe "Posts", type: :system do
   		login(user)
   	end
   	context "フォームの入力値が正常の場合" do
-  		it "ページ遷移が正常" do
-  			
-  		end
   		it "新規投稿が成功する" do
   			visit new_post_path
   			fill_in "投稿内容", with: "テスト"
   			fill_in "タグ付け", with: "テスト"
   			attach_file "post-image", "#{Rails.root}/spec/fixtures/test.png"
   			click_button "投稿"
-  			expect(page).to have_content "テスト"
+  			expect(page).to have_content "投稿しました"
   		end
   	end
   	context "フォームの値が不正の場合" do
@@ -49,6 +46,27 @@ RSpec.describe "Posts", type: :system do
       click_link "like-btn"
       expect(find('#like-off')).to be_visible
     end
+  end
+  describe "投稿詳細ページのテスト" do
+    let!(:post) {create(:post)}
+    before do
+      login(post.user)
+    end
+    context "ログインユーザーの投稿の場合" do
+      it "削除ボタンが表示される" do
+        visit post_path(post)
+        expect(page).to have_link "削除"
+      end
+    end
+    context "ログインユーザー以外の投稿の場合" do
+      let(:user2) {create(:user, name:"user2", acount_name:"@test_user2", email: "user2@example.com")}
+      let(:post2) {create(:post, user: user2)}
+      it "ログインユーザー以外の投稿の場合" do
+        visit post_path(post2)
+        expect(page).not_to have_link "削除"
+      end
+    end
+
   end
 
 end
