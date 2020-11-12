3d90e4f66b332b6de9e661c26a13966c02bcf135
vagrant <gomisuke1998@gmail.com>
2020-11-12 08:18:39 +0000

    fix:test:user_sample_user

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "Users", type: :system do
         attach_file "profile-image", "#{Rails.root}/spec/fixtures/test.png"
         click_button "新規登録"
       end
-  		it "サクセスメッセージが表示される" do
+  		it "登録完了メッセージが表示される" do
   			expect(page).to have_content "アカウント登録が完了しました"
   		end
       it "ホーム画面に遷移する" do
@@ -57,7 +57,7 @@ RSpec.describe "Users", type: :system do
         fill_in "パスワード", with: "000000"
         click_button "ログイン"
       end
-  		it "サクセスメッセージが表紙される" do
+  		it "ログイン完了メッセージが表示される" do
   			expect(page).to have_content "ログインしました"
   		end
       it "ホームページに遷移する" do
@@ -103,17 +103,24 @@ RSpec.describe "Users", type: :system do
 
   describe "サンプルユーザーのテスト" do
     let!(:user) {create(:user, email: "test_user@example.com")}
-    it "簡単ログインのテスト" do
+    before do
       visit root_path
       click_link "test-signin-btn"
-      expect(page).to have_content "サンプルユーザーとしてログインしました。"
     end
-    it "サンプルユーザーは会員情報を編集できない" do
-      visit root_path
-      click_link "test-signin-btn"
-      visit edit_user_registration_path
-      click_button "登録情報を更新する"
-      expect(page).to have_content "サンプルユーザーは変更できません。"
+    describe "簡単ログインのテスト" do
+      it "ログインメッセージが表示される" do
+        expect(page).to have_content "サンプルユーザーとしてログインしました。"
+      end
+      it "ホームページに遷移する" do
+        expect(find('#home-image')).to be_visible
+      end
+    end
+    describe "サンプルユーザー編集機能のテスト" do
+      it "サンプルユーザーは会員情報を編集できない" do
+        visit edit_user_registration_path
+        click_button "登録情報を更新する"
+        expect(page).to have_content "サンプルユーザーは変更できません。"
+      end
     end
   end
 end
